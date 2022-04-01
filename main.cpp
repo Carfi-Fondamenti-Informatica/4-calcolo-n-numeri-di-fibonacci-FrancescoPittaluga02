1866556692d3cc9f89a7c5d6a20d0a8a423754f5
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-04-01 04:23:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
+
+
 int main() {
-   return 0;
+    int n=0,i=0,primo_numero=0,secondo_numero=1,numero_successivo=0;
+    cin>>n;
+    if(n>2){
+       for (int i=2;i<n;i++){
+           numero_successivo=primo_numero+secondo_numero;
+           cout<<numero_successivo<<endl;
+           primo_numero=secondo_numero;
+           secondo_numero=numero_successivo;
+
+       }
+
+    }else {
+        cout<<"ERRORE"<<endl;
+    }
+
+    return 0;
 }
